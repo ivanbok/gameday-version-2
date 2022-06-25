6fb93ddd44c3c69aabbfcb504efb0b4efda4345a
Ivan Bok <bokivan@hotmail.com>
2022-06-25 17:16:00 +0800

    Initial commit

---
+++ lambda/getbetresults.py
@@ -0,0 +1,57 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+client = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+  # country = event["queryStringParameters"]["country"]
+  # racedatetime = event["queryStringParameters"]["racedatetime"]
+  # username = event["queryStringParameters"]["username"]
+  
+  ## For Testing
+  country = 'singapore'
+  race_datetime = '202206241200'
+  username = "ivan"
+  
+  ## API Gateway Parameters
+  # country=singapore&starttime=202206041000&endtime=202206052000
+  
+  data = client.query(
+    TableName='gamedaybettingresults',
+    KeyConditionExpression='username = :username AND race_datetime = :race_datetime',
+    ExpressionAttributeValues={
+      ':username': {
+        'S': username
+      },
+      ':race_datetime': {
+        'N': race_datetime
+      },
+    }
+  )
+  
+  if data["Count"] == 0:
+    body = json.dumps({})
+  else:
+    bet_result = []
+    for item in data["Items"]:
+        if item["country"]["S"] == country:
+            bet_result.append({"country": item["country"]["S"], 
+                "race_datetime": item["race_datetime"]["N"], 
+                "country": item["country"]["S"],
+                "bet_amount": item["bet_amount"]["N"],
+                "winnings": item["winnings"]["N"],
+                "bet_type": item["bet_type"]["S"],
+            })
+    body = json.dumps(bet_result)
+
+  response = {
+      'statusCode': 200,
+      'body': body,
+      'headers': {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+  }
+  
+  return response

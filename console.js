b1e8e1941695e331f8b3bd2e80db272f17f98d8e
Ivan Bok <bokivan@hotmail.com>
2022-06-25 21:01:22 +0800

    Added login and logout feature

--- console.js
+++ console.js
@@ -6,7 +6,7 @@ var poolData = {
 var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 userPool.getCurrentUser().getSession(function(err, session) {
     console.log(session.getIdToken().getJwtToken());
-    fetch('https://2foxz7t1qb.execute-api.ap-southeast-1.amazonaws.com/prod/betresults?username=${userPool.getCurrentUser().username}', {
+    fetch(`https://2foxz7t1qb.execute-api.ap-southeast-1.amazonaws.com/prod/betresults?username=${userPool.getCurrentUser().username}`, {
     method: 'GET', // or 'PUT'
     headers: {
         'Authorization': session.getIdToken().getJwtToken(),

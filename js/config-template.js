58cbe99237d0884ab50b99f038be5a8f0867cfee
Ivan Bok <bokivan@hotmail.com>
2022-06-27 10:37:31 +0800

    CFN template for testing

--- js/config-template.js
+++ js/config-template.js
@@ -1,6 +1,6 @@
 var poolData = {
-    UserPoolId: 'ap-southeast-1_s2MKiz8lv',
-    ClientId: '6scpclneda95etff5qvfq0rsqa'
+    UserPoolId: 'replacethiswiththeuserpoolid',
+    ClientId: 'replacethiswiththeclientid'
 };
 
 var apiurl = "replacethiswiththeapiurl";
